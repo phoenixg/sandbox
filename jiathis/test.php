7f71ba5b269638cbb5076dda4f088c91169c871d
phoenix <306761352@qq.com>
2012-12-13 12:45:08 +0800

    add jiathis

---
+++ jiathis/test.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+
+<div class="jiathis_style">
+	<span class="jiathis_txt">分享到：</span>
+	<a class="jiathis_button_tsina">新浪微博</a>
+	<a class="jiathis_button_tqq">腾讯微博</a>
+	<a class="jiathis_button_feixin">飞信</a>
+	<a class="jiathis_button_douban">豆瓣</a>
+	<a class="jiathis_button_qzone">QQ空间</a>
+	<a class="jiathis_button_copy">复制网址</a>
+	<a class="jiathis_counter_style"></a>
+</div>
+
+
+<div id="article_title" style="display:none;">微博内容</div>
+<div id="description" style="display:none;">摘要</div>
+
+<script>
+var jiathis_config = {
+	url:"http://www.jiathis.com",
+	title:document.getElementById("article_title").innerHTML,
+	//summary:document.getElementById("description").innerHTML
+}
+</script>
+
+<!-- 
+放到底部
+我自己的uid
+-->
+<script type="text/javascript" src="http://v3.jiathis.com/code_mini/jia.js?uid=1355207165741482" charset="utf-8"></script>
+</body>
+</html>
