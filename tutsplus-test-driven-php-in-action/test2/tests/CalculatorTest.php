68f875db14384aa198478aa96185f526049eef4a
phx-hp1000 <306761352@qq.com>
2013-03-19 19:27:17 +0800

    a

---
+++ tutsplus-test-driven-php-in-action/test2/tests/CalculatorTest.php
@@ -0,0 +1,16 @@
+<?php 
+// phpunit CalculatorTest.php
+// 有几个public function ，就有几个test，function前面必须以"test"开头
+
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+	public function testSomething()
+	{
+		
+	}
+
+	public function testAnotherThing()
+	{
+		
+	}
+}
