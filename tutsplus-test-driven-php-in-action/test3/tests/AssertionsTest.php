894a990447426e1f623402189f1199780c5dec7f
phx-hp1000 <306761352@qq.com>
2013-03-19 20:22:29 +0800

    a

---
+++ tutsplus-test-driven-php-in-action/test3/tests/AssertionsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class AssertionsTest extends PHPUnit_Framework_TestCase
+{
+	public function testBasicAssertion()
+	{
+		//$this->assertTrue( 1 + 1 === 3, 'It should be 2' );
+		//$this->assertEquals( 1 + 1, 2 );
+		//$this->assertTrue(new DateTime() instanceof DateTime);
+		//$this->assertInstanceOf('DateTime', new DateTime());
+
+		/*
+		$arr = array();
+		$this->assertCount(0, $arr);
+		$arr[] = 'a';
+		$this->assertCount(1, $arr);
+		$this->assertContains('a', $arr);
+		$this->assertNotContains('b', $arr);
+		array_pop($arr);
+		$this->assertCount(0, $arr);
+		*/
+		
+	}
+
+}
