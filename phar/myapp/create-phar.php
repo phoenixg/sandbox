023988579852c4df7d16dbeab8b79770a1a056be
phx-hp1000 <306761352@qq.com>
2013-03-25 22:06:48 +0800

    a

--- phar/myapp/create-phar.php
+++ phar/myapp/create-phar.php
@@ -1,10 +1,13 @@
 <?php
+
+ini_set("phar.readonly", 0);
+
 $srcRoot = "../myapp/src";
 $buildRoot = "../myapp/build";
  
 $phar = new Phar($buildRoot . "/myapp.phar", 
-	FilesystemIterator::CURRENT_AS_FILEINFO |     	FilesystemIterator::KEY_AS_FILENAME, "myapp.phar");
-$phar["index.php"] = file_get_contents($srcRoot . "/index.php");
+	FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, "myapp.phar");
+$phar["index.php"]  = file_get_contents($srcRoot . "/index.php");
 $phar["common.php"] = file_get_contents($srcRoot . "/common.php");
 $phar->setStub($phar->createDefaultStub("index.php"));
 
